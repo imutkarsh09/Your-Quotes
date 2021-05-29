1ff33aac1f83448bc3c546353767ffa91c5fa2fd
imutkarsh09 <itsutkarsh2711@gmail.com>
2021-05-29 23:12:20 +0530

    Show Quotes Screen Added

--- lib/Navigations/showQuotes.dart
+++ lib/Navigations/showQuotes.dart
@@ -34,6 +34,7 @@ class _ShowQuotesState extends State<ShowQuotes> {
     print("Response Code ${response.statusCode}");
     print("Genre---> ${widget.genre}");
     print("Author----> ${widget.author}");
+    print("Data Length-->${data.length}");
     if (response.statusCode == 7) {
       print("No Internet");
     }
@@ -52,17 +53,82 @@ class _ShowQuotesState extends State<ShowQuotes> {
     return data.length == 0
         ? Loader()
         : Scaffold(
-            appBar: AppBar(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  page = page + 1;
-                  getJsonData();
-                });
-              },
-            ),
-            body: Center(
-              child: Text(data[0]["quoteText"]),
-            ));
+            // appBar: AppBar(),
+            body: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  fit: BoxFit.cover,
+                  colorFilter: new ColorFilter.mode(
+                      Colors.black.withOpacity(0.9), BlendMode.dstATop),
+                  image: AssetImage("Images/Background_Images/Background.jpg"),
+                )),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: double.infinity,
+                color: Colors.red,
+                child: Column(
+                  children: [
+                    SafeArea(
+                      child: Container(
+                          height: 40,
+                          width: double.infinity,
+                          color: Colors.yellow,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          )),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height / 1.4,
+                      width: double.infinity,
+                      color: Colors.blue,
+                      child: Wrap(
+                        children: [
+                          Text(
+                            data[0]["quoteText"],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: "Baloo",
+                              fontSize: 25,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_left),
+                          onPressed: () {
+                            page = page + 1;
+                            getJsonData();
+                          },
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            if (page > 1) {
+                              page = page - 1;
+                              getJsonData();
+                            }
+                          },
+                          icon: Icon(Icons.arrow_right),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ));
   }
 }
